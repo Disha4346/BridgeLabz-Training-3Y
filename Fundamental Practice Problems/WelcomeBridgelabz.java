9f39d2f39b68d457d1c4dcf840cbc9739a612289
Disha4346 <143337154+Disha4346@users.noreply.github.com>
2025-08-31 02:50:56 +0530

    Delete Fundamental Practice Problems directory

--- Fundamental Practice Problems/WelcomeBridgelabz.java
+++
@@ -1,8 +0,0 @@
-/**
- * Prints a welcome message.
- */
-public class WelcomeBridgelabz {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Bridgelabz!");
-    }
-}
